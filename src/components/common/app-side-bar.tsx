180a8df87d92455a286dd979042eb2204ccf829a
leegyuseung <lgs4002@naver.com>
2026-01-21 21:16:55 +0900

    fix : 사이드 메뉴 수정
    UI 수정 및 필수메뉴만 남겨두고 삭제, live 폴더 추가

--- src/components/common/app-side-bar.tsx
+++ src/components/common/app-side-bar.tsx
@@ -1,43 +1,41 @@
+"use client";
 import Link from "next/link";
-import {
-  CalendarCheck,
-  Flame,
-  LayoutDashboard,
-  Tv,
-  Users,
-  Video,
-} from "lucide-react";
+import { Users, Menu, TvMinimalPlay } from "lucide-react";
 import {
   Sidebar,
   SidebarContent,
   SidebarGroup,
   SidebarGroupContent,
-  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-  SidebarTrigger,
+  useSidebar,
 } from "../ui/sidebar";
 
 // 메뉴 구성 데이터
 const menuItems = [
-  { title: "홈", url: "/", icon: LayoutDashboard },
-  { title: "버츄얼 리스트", url: "/vlist", icon: Users },
-  { title: "콘텐츠 모아보기", url: "/contents", icon: Video },
-];
-
-const groupItems = [
-  { title: "아이돌 그룹", url: "/group/idol", icon: Flame },
-  { title: "크루 그룹", url: "/group/crew", icon: Tv },
+  { title: "라이브", url: "/live", icon: TvMinimalPlay },
+  { title: "리스트", url: "/vlist", icon: Users },
 ];
 
 export default function AppSideBar() {
+  const { toggleSidebar } = useSidebar();
   return (
-    <Sidebar variant="sidebar" collapsible="icon">
-      <SidebarHeader>
-        <SidebarTrigger />
-      </SidebarHeader>
-      <SidebarContent>
+    <Sidebar variant="sidebar" collapsible="icon" className="border-none">
+      <SidebarContent className="bg-white border-r-0 flex items-center">
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem onClick={toggleSidebar}>
+                <SidebarMenuButton className="cursor-pointer">
+                  <Menu className="w-5 h-5" />
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        {/* 첫 번째 메뉴그룹 */}
         <SidebarGroup>
           <SidebarGroupContent>
             <SidebarMenu>
@@ -51,39 +49,6 @@ export default function AppSideBar() {
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-
-        {/* 그룹 메뉴 */}
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {groupItems.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild tooltip={item.title}>
-                    <Link href={item.url}>
-                      <item.icon className="w-5 h-5" />
-                      <span>{item.title}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <Link href="/quest">
-                    <CalendarCheck className="w-5 h-5 text-yellow-500" />
-                    <span>출석 체크 & 포인트</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
